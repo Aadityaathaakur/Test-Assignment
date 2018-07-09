5d072de96beb49a3a93a854a36553996ad56c7c1
Aditya Pratap Singh <40260446+Aadityaathaakur@users.noreply.github.com>
2018-07-09 17:59:24 +0530

    Update TestClass.java

--- testLink/src/test/java/testLink/TestClass.java
+++ testLink/src/test/java/testLink/TestClass.java
@@ -1,12 +1,15 @@
 package testLink;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class TestClass {
-
+	//WebDriver driver=ActionClass.driver;
+	
 	@BeforeClass
 	public void launch()
 	{
@@ -14,7 +17,7 @@ public class TestClass {
 	}
 	@Test
 	public void test001_basic_auth_test() throws InterruptedException{
-		//ActionClass.basic_auth();
+		ActionClass.basic_auth();
 	}
 	
 	@Test
@@ -27,12 +30,15 @@ public class TestClass {
 		ActionClass.Exit_Intent();
 	}
 	
+	
+	
+	
 	@Test
 	public  void test004_Sortable_Data_Tables() {
 		ActionClass.Sortable_Data_Tables();
 		
 	}
-	 @Test
+	  @Test
 	   public void test005_authenticate(){
 		
 		   Assert.assertTrue(ActionClass.verify_login_page());
@@ -50,9 +56,55 @@ public class TestClass {
 		 
 		   String element= ActionClass.verify_valid_credentiails();
 		   System.out.println(element);
-		   Assert.assertEquals(element, "You logged out of the secure area!"
-		   		+ "×");
+		   Assert.assertEquals(element, "Youloggedoutofthesecurearea!×");
+	   }
+	   
+	
+	   @Test
+	   public void test008_test_For_3Images_Displayed() throws InterruptedException{
+		 
+		   int size = ActionClass.test_For_3Images_Displayed();
+		   System.out.println(size);
+		   
+		   Assert.assertEquals(size, 3);
+	   }
+	   
+	   @Test
+	   public void test009_Hover_mouse_over(){
+		   
+		   WebElement details= ActionClass.Hover_mouse_over();
+		 
+		   //Assert.assertTrue(driver.findElement(By.xpath("//*[contains(text(),'name: user1')]")).isDisplayed());
+		   
+		   Assert.assertTrue(details.isDisplayed());
+				   
+//		   Assert.assertEquals(uname, "name: user1");
+//		   Assert.assertEquals(viewProfile, "View profile");
+		   
+	   }
+	   
+	
+	 @Test
+	   public void test010_WYSIWYG_Editor_Visible() throws InterruptedException
+	   {	WebElement element=ActionClass.WYSIWYG_Editor_Visible();
+	   Assert.assertTrue(element.isDisplayed());
+		   
 	   }
 	
+	   @Test
+	   public void test011_WYSIWYG_Editor() throws InterruptedException
+	   {	
+		    String  text= ActionClass.WYSIWYG_Editor();
+		    Assert.assertEquals(text, "strong");
+		   
+	   }
+	   
+	   @Test
+	   public void test012_Status_Codes() throws InterruptedException
+	   {
+		   String text= ActionClass.Status_Codes();
+		   
+		   Assert.assertEquals(text,"Thispagereturneda404statuscode.ForadefinitionandcommonlistofHTTPstatuscodes,gohere");
+	   }
 	
 }
